dc1da13f33e45938e77d1918ff4b45b13b88341e
Timendus <mail@timendus.com>
2025-11-21 01:32:27 +0100

    This may or may not be better :)

--- client/js/service-worker.js
+++ client/js/service-worker.js
@@ -19,26 +19,40 @@ const filesToCache = [
   "sample-schedule.json",
 ];
 
+// Prefetch everything in the above array and store in the cache on install
 self.addEventListener("install", function (evnt) {
   evnt.waitUntil(
     caches.open("clock").then((cache) => {
-      return cache.addAll(filesToCache.map((file) => `./${file}`));
+      return cache.addAll(filesToCache.map((file) => `../${file}`));
     })
   );
 });
 
-// This should, I hope, serve requests from the cache first and then update the
-// cache from the network in the background.
+// This should serve requests from the cache first and then update the cache
+// from the network in the background.
 self.addEventListener("fetch", function (evnt) {
+  if (evnt.request.method !== "GET") {
+    return; // Let the browser handle other requests
+  }
+
   evnt.respondWith(
     caches.open("clock").then((cache) => {
       return cache.match(evnt.request).then((cachedResponse) => {
-        const fetchedResponse = fetch(evnt.request).then((networkResponse) => {
-          cache.put(evnt.request, networkResponse.clone());
+        // cache.match always resolves, with either a response or undefined.
 
-          return networkResponse;
-        });
+        // Fetch the requested resource in parallel
+        const fetchedResponse = fetch(evnt.request)
+          .then((networkResponse) => {
+            // Cache the network response for the next request
+            cache.put(evnt.request, networkResponse.clone());
+            return networkResponse;
+          })
+          // This is probably BS, but ChatGPT thinks this prevents us from
+          // throwing too many errors in the console. Kinda hard to validate...
+          .catch(() => cachedResponse);
 
+        // Return from cache immediately on cache hit or return the network
+        // request promise on cache miss
         return cachedResponse || fetchedResponse;
       });
     })
